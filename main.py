a96f76bb636acb723e52407a2c912357b63a5556
dequhet <arsenii.kopylov.2008@gmail.com>
2025-02-21 20:41:17 +0300

    123

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
 
-from fastapi import FastAPI
-
+import logging 
+from fastapi import FastAPI,Request
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename='logs.txt')
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/ping")
-async def ping():
+async def ping(request:Request):
+    client_host = request.client.host
+    logger.info(f"Получен запрос от {client_host}")
     return {"status": "ok"}
